9fabadc1442b1ca903791ed66bca2495e346a3cf
Amadeus710 <69098664+Amadeus710@users.noreply.github.com>
2022-07-15 15:45:08 -0400

    use api

--- routes/auth.js
+++ routes/auth.js
@@ -20,10 +20,10 @@ router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email })
         if (!user) return res.status(404).send('いないよ')
-        
+
         const vailedPassword = req.body.password === user.password
         if (!vailedPassword) return res.status(400).json('ちがうよ')
-        
+
         return res.status(200).json(user)
     } catch (err) {
         return res.status(500).json(err)
